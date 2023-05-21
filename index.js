e2632074c3172643dbeb9041270877695d59f0f8
Nikita <122488419+NikitaKxm@users.noreply.github.com>
2023-05-21 19:40:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+});
+
+function toggleDropdown() {
+  let dropdownMenu = document.getElementById('dropdownMenu');
+  dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+let currentDateElement = document.getElementById('copyright');
+
+  // Оновлюємо дату в елементі футера
+  function updateCurrentDate() {
+    let currentDate = new Date();
+    currentDateElement.textContent = currentDate.getFullYear() + " website by rayane saada"; // 
+  }
+  updateCurrentDate();
